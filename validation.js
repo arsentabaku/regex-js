d998a5ca7c35d0beb9315231f5acfff42c9129b1
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2021-07-25 18:43:07 +0200

    Added an event to each form elements and added a class based on regex validity

--- validation.js
+++ validation.js
@@ -1,1 +1,26 @@
 const inputs = document.querySelectorAll("input");
+
+const patterns = {
+  telephone: /^\d{11}$/i
+};
+
+function addEventListenersToFormElements() {
+  inputs.forEach((input) => {
+    input.addEventListener("keyup", (e) => {
+      addClassToTheFieldBasedOnValidityOfUserInput(
+        e.target,
+        patterns[e.target.attributes.name.value]
+      );
+    });
+  });
+}
+
+function addClassToTheFieldBasedOnValidityOfUserInput(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+addEventListenersToFormElements();
