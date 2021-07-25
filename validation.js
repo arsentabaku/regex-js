e5b27069f8e9106fdac867268167c7e24f34d3fd
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2021-07-25 18:43:34 +0200

    Added the regex patterns for the rest of the form elements

--- validation.js
+++ validation.js
@@ -1,7 +1,11 @@
 const inputs = document.querySelectorAll("input");
 
 const patterns = {
-  telephone: /^\d{11}$/i
+  username: /^[a-zA-Z\d]{5,12}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  password: /^[\w@-]{8,20}$/,
+  telephone: /^\d{11}$/i,
+  slug: /^[a-z\d-]{8,20}$/,
 };
 
 function addEventListenersToFormElements() {
